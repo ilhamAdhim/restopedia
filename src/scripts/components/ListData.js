e2f8b5591a60746508435b7eb1b924350a194acb
ilhamadhim <ilhamm179@gmail.com>
2022-01-30 15:17:33 +0700

    updated

--- src/scripts/components/ListData.js
+++ src/scripts/components/ListData.js
@@ -1,13 +1,50 @@
+import dataJSON from "../../DATA.json";
+
 class ListData extends HTMLElement {
+  constructor() {
+    super();
+  }
+
   connectedCallback() {
+    this._restaurants = dataJSON?.restaurants;
     this._render();
   }
 
   _render() {
-    console.log("ya ini data list");
-    this.innerHTML = `
-          
-          `;
+    let delay = 0;
+    this._restaurants?.forEach((restaurant) => {
+      console.log(restaurant);
+      delay += 50;
+      //   let createRestaurantElement = document.createElement("list-item");
+      //   createRestaurantElement.restaurant = restaurant;
+      //   createRestaurantElement.delayValue = delay;
+
+      //   this.appendChild(createRestaurantElement);
+      this.className = "card-list";
+      //   Display only first 20 words as the description
+      this.description = restaurant?.description
+        ?.split(" ")
+        .slice(0, 20)
+        .join(" ");
+
+      this.innerHTML += `
+      <div class="card">
+        <img class="card-image" src="${
+          restaurant?.pictureId
+        }" alt="${`Image of ${restaurant?.name}`}">
+        <div class="card-title">${restaurant?.name}</div>
+        <div class="card-body">
+            <div class="card-description">${`${this.description} . . .`} </div>
+        </div>
+        <div class="city-info">
+        <div class="col">
+            <i class="fa fa-map-marker icon"></i>
+            <span> ${restaurant?.city}</span>
+        </div>
+        
+        </div>
+      </div>`;
+    });
   }
 }
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,11 +4,11 @@ import "../styles/footer.scss";
 import "../styles/buttons.scss";
 import "../styles/navbar.scss";
 import "../styles/hero.scss";
+import "../styles/card-item.scss";
 
 import "../scripts/components/navbar.js";
 import "../scripts/components/hero.js";
 import "../scripts/components/ListData.js";
-import "../scripts/components/ListItem.js";
 import { navbar } from "./setnavbar.js";
 
 navbar();
